e982b9ecaa1103e777877115d8f4c5cc154c3a40
xblaise <ixblaise@gmail.com>
2015-04-21 17:19:36 -0700

    Changes with all new images/tabs

--- includes/js/rescue.js
+++ includes/js/rescue.js
@@ -3,6 +3,7 @@ Rescue JS
 Code and stuff you need for the Rescue template
 */
 
+
 jQuery(function($) {
 	
 	$(window).load(function(){
@@ -67,3 +68,19 @@ jQuery(document).ready(function($){
 	$(".lightbox").attr('rel', 'gallery').fancybox();
 
 });
+
+jQuery(document).ready(function() {
+	jQuery('.tabs .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
+
+		// Show/Hide Tabs
+		jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+		// Change/remove current tab to active
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
+});
+
+
